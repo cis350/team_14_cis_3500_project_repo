285a14da76b2b77fb87b528305b75461a1ea5e50
Albinator3000 <albert.w.opher.iv@gmail.com>
2024-05-06 15:42:18 -0400

    E2E testing

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: '6fy9yf',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
